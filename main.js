1bfb1e244948266d3f6765888668046f95a4912c
luis henrique pontes <luishenrique.pontes@yahoo.com.br>
2021-08-10 21:33:40 -0300

    criando formularios de inscriçao, filoiação, pesquisa e deshboard para administrador

--- main.js
+++ main.js
@@ -136,3 +136,41 @@ window.addEventListener('scroll', function () {
   backToTop()
   activateMenuAtCurrentSection()
 })
+
+/* Máscaras ER VALIDAÇÃO DE TELEFONE*/
+function mascara(o, f) {
+  v_obj = o
+  v_fun = f
+  setTimeout('execmascara()', 1)
+}
+function execmascara() {
+  v_obj.value = v_fun(v_obj.value)
+}
+
+function mtel(v) {
+  v = v.replace(/\D/g, '') //Remove tudo o que não é dígito
+  v = v.replace(/^(\d{2})(\d)/g, '($1) $2') //Coloca parênteses em volta dos dois primeiros dígitos
+  v = v.replace(/(\d)(\d{4})$/, '$1-$2') //Coloca hífen entre o quarto e o quinto dígitos
+  return v
+}
+
+function mtel1(v1) {
+  v1 = v1.replace(/\D/g, '') //Remove tudo o que não é dígito
+  v1 = v1.replace(/(\d)(\d{3})$/, '$1-$2') //Coloca hífen entre o quarto e o quinto dígitos
+  return v1
+}
+
+function id(el) {
+  return document.getElementById(el)
+}
+window.onload = function () {
+  ;(id('telefone').onkeyup = function () {
+    mascara(this, mtel)
+  }),
+    (id('cep').onkeyup = function () {
+      mascara(this, mtel1)
+    }),
+    (id('whatsapp').onkeyup = function () {
+      mascara(this, mtel)
+    })
+}
